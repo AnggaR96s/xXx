ebe1591355f9c04e9050fc3e5f002d561defe9c3
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2018-10-07 22:09:41 +0530

    Add count of dialogs, inspired by @rafalense

---
+++ stdplugins/count.py
@@ -0,0 +1,40 @@
+from telethon import events
+import asyncio
+from datetime import datetime
+from telethon.tl.types import User, Chat, Channel
+
+
+@borg.on(events.NewMessage(pattern=r"\.count", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    u = 0
+    g = 0
+    c = 0
+    bc = 0
+    b = 0
+    dialogs = await borg.get_dialogs(
+        limit=None,
+        ignore_migrated=True
+    )
+    for d in dialogs:
+        currrent_entity = d.entity
+        if type(currrent_entity) is User:
+            if currrent_entity.bot:
+                b += 1
+            else:
+                u += 1
+        elif type(currrent_entity) is Chat:
+            g += 1
+        elif type(currrent_entity) is Channel:
+            if currrent_entity.broadcast:
+                bc += 1
+            else:
+                c += 1
+        else:
+            print(d)
+    end = datetime.now()
+    ms = (end - start).seconds
+    await event.edit("Obtained in {} seconds.\nUsers:\t{}\nGroups:\t{}\nSuper Groups:\t{}\nChannels:\t{}\nBots:\t{}".format(ms, u, g, c, bc, b))
+
