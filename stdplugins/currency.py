2eb2ab4d3043bb91929869695aac9bb0f55b8da8
Shrimadhav U K <shrimadhavuk@gmail.com>
2018-10-13 20:28:36 +0530

    initiate currency conversion. Courtesy: https://exchangeratesapi.io/

---
+++ stdplugins/currency.py
@@ -0,0 +1,32 @@
+from telethon import events
+import asyncio
+from datetime import datetime
+import requests
+
+
+@borg.on(events.NewMessage(pattern=r"\.currency (.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    input_str = event.pattern_match.group(1)
+    input_sgra = input_str.split(" ")
+    if len(input_sgra) == 3:
+        try:
+            number = float(input_sgra[0])
+            currency_from = input_sgra[1].upper()
+            currency_to = input_sgra[2].upper()
+            request_url = "https://api.exchangeratesapi.io/latest?base={}".format(currency_from)
+            current_response = requests.get(request_url).json()
+            if currency_to in current_response["rates"]:
+                current_rate = float(current_response["rates"][currency_to])
+                rebmun = number * current_rate
+                await event.edit("{} {} = {} {}".format(number, currency_from, rebmun, currency_to))
+            else:
+                await event.edit("IDEKNOWTDWTT")
+        except e:
+            await event.edit(str(e))
+    else:
+        await event.edit("`.currency number from to`")
+    end = datetime.now()
+    ms = (end - start).seconds
