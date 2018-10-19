d144c2d3546bb767cf0c3430fdd4a761cd4c1e62
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2018-10-20 00:12:43 +0530

    fix temporary issues

--- stdplugins/telegraph.py
+++ stdplugins/telegraph.py
@@ -8,15 +8,17 @@ short_name = os.environ.get("TELEGRAPH_SHORT_NAME", "UniBorg")
 PRIVATE_GROUP_BOT_API_ID = borg.uid
 
 
+telegraph = Telegraph()
+r = telegraph.create_account(short_name=short_name)
+auth_url = r["auth_url"]
+
+
 @borg.on(events.NewMessage(pattern=r"\.telegraph (media|text)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
     if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TMP_DOWNLOAD_DIRECTORY)
-    telegraph = Telegraph()
-    r = telegraph.create_account(short_name=short_name)
-    auth_url = r["auth_url"]
     await borg.send_message(
         PRIVATE_GROUP_BOT_API_ID,
         "Created New Telegraph account {} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**".format(auth_url)
@@ -45,9 +47,11 @@ async def _(event):
                 os.remove(downloaded_file_name)
                 await event.edit("Uploaded to https://telegra.ph/{} in {} seconds.".format(media_urls[0], (ms + ms_two)))
         elif input_str == "text":
-            user_object = await borg.get_entity(event.message.from_id)
-            title_of_page = user_object.first_name + " " + user_object.last_name
+            user_object = await borg.get_entity(r_message.from_id)
+            title_of_page = user_object.first_name # + " " + user_object.last_name
+            # apparently, all Users do not have last_name field
             page_content = r_message.message
+            page_content = page_content.replace("\n", "<br>")
             response = telegraph.create_page(
                 title_of_page,
                 html_content=page_content
