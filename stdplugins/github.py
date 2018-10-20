7347ddc98d3c7608577c51a5546bc1f2d2b339bc
Shrimadhav U K <shrimadhavuk@gmail.com>
2018-10-20 12:54:55 +0530

    Add GitHub API

---
+++ stdplugins/github.py
@@ -0,0 +1,34 @@
+from telethon import events
+import asyncio
+import requests
+import json
+
+
+@borg.on(events.NewMessage(pattern=r"\.github (.*)"))
+@borg.on(events.MessageEdited(pattern=r"\.github (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    url = "https://api.github.com/users/{}".format(input_str)
+    r = requests.get(url)
+    if r.status_code != 404:
+        b = r.json()
+        avatar_url = b["avatar_url"]
+        html_url = b["html_url"]
+        gh_type = b["type"]
+        name = b["name"]
+        company = b["company"]
+        blog = b["blog"]
+        location = b["location"]
+        bio = b["bio"]
+        created_at = b["created_at"]
+        await event.edit("""[\u2063]({})Name: [{}]({})
+Type: {}
+Company: {}
+Blog: {}
+Location: {}
+Bio: {}
+Profile Created: {}""".format(avatar_url, name, html_url, gh_type, company, blog, location, bio, created_at))
+    else:
+        await event.edit("`{}`: {}".format(input_str, r.text))
--- stdplugins/tagall.py
+++ stdplugins/tagall.py
@@ -9,10 +9,8 @@ from telethon import events
 async def _(event):
     if event.fwd_from:
         return
-    await event.delete()
     mentions = "@tagall"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
-    await borg.send_message(
-        chat, mentions, reply_to=event.message.reply_to_msg_id)
+    await event.edit(mentions)
