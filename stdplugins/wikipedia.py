de00dc7e3c4ec3f3eeaae7cc838ddbbeb47062fd
Shrimadhav U K <shrimadhavuk@gmail.com>
2018-10-21 11:23:00 +0530

    Add wikimedia plugin

--- stdplugins/wikipedia.py
+++ stdplugins/wikipedia.py
@@ -6,7 +6,7 @@ from telethon import events
 import wikipedia
 
 
-@borg.on(events.NewMessage(pattern=r"\.wiki (.*)", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.wikipedia (.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
