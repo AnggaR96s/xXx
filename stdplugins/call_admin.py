7cd9c611e1d53f8749601b68f127fa4389c4b7b6
Shrimadhav U K <scaleway@shrimadhavuk.me>
2019-01-09 15:49:06 +0000

    Fix admin report command

--- stdplugins/call_admin.py
+++ stdplugins/call_admin.py
@@ -1,7 +1,10 @@
 from telethon import events
+import asyncio
 
-@borg.on(events.NewMessage(pattern=r"\.call\_admin", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\/admin", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
     await event.edit("@admin")
+    await asyncio.sleep(5)
+    await event.delete()
