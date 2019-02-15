12a7d5adaed7b505bc82ea445ad66bf3e77f9f2e
nitanmarcel <nitan.marcel@gmail.com>
2019-02-15 21:30:32 +0530

    ReWrite exec command

--- stdplugins/exec.py
+++ stdplugins/exec.py
@@ -2,37 +2,53 @@ from telethon import events
 import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError
 import os
+import asyncio
+import time
 
 
 @borg.on(events.NewMessage(pattern=r"\.exec (.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    input_str = event.pattern_match.group(1)
-    input_command = input_str.split(" ")
-    try:
-        t_response = subprocess.check_output(input_command, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as exc:
-        final_output = "**EXEC**: `{}` \nprocess returned {}\n output: {}".format(input_str, exc.returncode, exc.output)
-    else:
-        x_reponse = t_response.decode("UTF-8")
-        final_output = "**EXEC**: `{}` \n\n**OUTPUT**: \n{} \n".format(input_str, x_reponse)
-    if len(final_output) > Config.MAX_MESSAGE_SIZE_LIMIT:
-        if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-            os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-        current_file_name = "{}exec.text".format(Config.TMP_DOWNLOAD_DIRECTORY)
-        file_ponter = open(current_file_name, "w+")
-        file_ponter.write(final_output)
-        file_ponter.close()
-        await borg.send_file(
-            event.chat_id,
-            current_file_name,
-            force_document=True,
-            allow_cache=False,
-            caption=input_str,
-            reply_to=event.message.id
-        )
-        await event.delete()
-        os.remove(current_file_name)
-    else:
-        await event.edit(final_output)
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    OUTPUT = f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**Output:**\n"
+    while process:
+        if time.time() > start_time:
+            if process:
+                process.kill()
+            await event.edit(f"{OUTPUT}\n__Process killed__: `Time limit reached`")
+            break
+        stdout = await process.stdout.readline()
+        if not stdout:
+            _, stderr = await process.communicate()
+            if stderr.decode():
+                OUTPUT += f"`{stderr.decode()}`"
+                await event.edit(OUTPUT)
+                break
+        else:
+            OUTPUT += f"`{stdout.decode()}`"
+        if len(OUTPUT) > Config.MAX_MESSAGE_SIZE_LIMIT:
+            if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+                os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+            current_file_name = "{}exec.text".format(Config.TMP_DOWNLOAD_DIRECTORY)
+            file_ponter = open(current_file_name, "w+")
+            file_ponter.write(final_output)
+            file_ponter.close()
+            await borg.send_file(
+                event.chat_id,
+                current_file_name,
+                force_document=True,
+                allow_cache=False,
+                caption=input_str,
+                reply_to=event.message.id
+            )
+            await event.delete()
+            os.remove(current_file_name)
+            break
+        else:
+            await event.edit(OUTPUT)
