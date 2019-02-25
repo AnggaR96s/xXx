b3a5213850262551cf93b6ffb07fe3c1a2f1a52c
Shrimadhav U K <scaleway@shrimadhavuk.me>
2019-02-25 15:26:58 +0000

    Fix .purge command

--- stdplugins/purge.py
+++ stdplugins/purge.py
@@ -2,7 +2,7 @@ from telethon import events
 import asyncio
 
 
-@borg.on(events.NewMessage(pattern="\.purge", outgoing=True))
+@borg.on(events.NewMessage(pattern="\.purge ?(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -13,9 +13,17 @@ async def _(event):
         for i in range(4000):
             shiiinabot += "\u2060"
         await event.edit(shiiinabot)
-        async for message in borg.iter_messages(event.chat_id, min_id=event.reply_to_msg_id):
+        from_user = None
+        input_str = event.pattern_match.group(1)
+        if input_str == "me":
+            from_user = await borg.get_entity(input_str)
+            logger.info(from_user)
+        async for message in borg.iter_messages(event.chat_id, min_id=event.reply_to_msg_id, from_user=from_user):
             i = i + 1
             msgs.append(message)
+            if len(msgs) == 100:
+                await borg.delete_messages(event.chat_id, msgs)
+                msgs = []
         if len(msgs) <= 100:
             await borg.delete_messages(event.chat_id, msgs)
             msgs = []
