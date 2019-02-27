b4ff95dbf960755ca9905332db4c713f3843d46d
Shrimadhav U K <scaleway@shrimadhavuk.me>
2019-02-27 15:07:34 +0000

    Introducing prank.promote

--- stdplugins/promote.py
+++ stdplugins/promote.py
@@ -38,3 +38,30 @@ async def _(event):
         await event.edit("Successfully Promoted")
         await asyncio.sleep(5)
         await event.delete()
+
+
+
+@borg.on(events.NewMessage(pattern=r"prank\.promote ?(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    to_promote_id = None
+    rights = ChatAdminRights(
+        post_messages=True
+    )
+    input_str = event.pattern_match.group(1)
+    reply_msg_id = event.message.id
+    if reply_msg_id:
+        r_mesg = await event.get_reply_message()
+        to_promote_id = r_mesg.sender_id
+    elif input_str:
+        to_promote_id = input_str
+    try:
+        await borg(EditAdminRequest(event.chat_id, to_promote_id, rights))
+    except (Exception) as exc:
+        await event.edit(str(exc))
+    else:
+        await event.edit("Successfully Promoted")
+        await asyncio.sleep(5)
+        await event.delete()
