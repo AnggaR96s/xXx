f966d8b1b3cf03b45fd3e6ddf0bf3f8094c2190e
Shrimadhav U K <scaleway@shrimadhavuk.me>
2019-03-03 08:08:40 +0000

    [COPY] speedtest from nitanmarcel

--- stdplugins/speedtest.py
+++ stdplugins/speedtest.py
@@ -1,35 +1,62 @@
 from telethon import events
-import subprocess, os
 from datetime import datetime
+import io
 import requests
+import speedtest
 
 
 @borg.on(events.NewMessage(pattern=r"\.speedtest", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
+    await event.edit("Calculating my internet speed. Please wait!")
     start = datetime.now()
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    await event.edit("Downloading speedtest-cli binary to my local ... Thus might take some time")
-    url = "https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py"
-    required_file_name = Config.TMP_DOWNLOAD_DIRECTORY + "speedtest.cli"
-    r = requests.get(url, stream=True)
-    with open(required_file_name, "wb") as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
-    await event.edit("Now executing python speedtest.py Please wait")
-    command_to_run = "python3 {}".format(required_file_name)
-    input_command = command_to_run.split(" ")
-    output_str = None
-    try:
-        t_response = subprocess.check_output(input_command, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as exc:
-        output_str = "process returned {}\n output: {}".format(exc.returncode, exc.output)
-    else:
-        x_reponse = t_response.decode("UTF-8")
-        output_str = x_reponse
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    os.remove(required_file_name)
-    await event.edit("**SpeedTest** completed in {} seconds \n\n {}".format(ms, output_str))
+    response = s.results.dict()
+    download_speed = response.get("download")
+    upload_speed = response.get("upload")
+    ping_time = response.get("ping")
+    reply_msg_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_msg_id = event.reply_to_msg_id
+    try:
+        response = requests.get(s.results.share())
+        with io.BytesIO(response.content) as speedtest_image:
+            speedtest_image.name = "speedtest.png"
+            await borg.send_file(
+                event.chat_id,
+                speedtest_image,
+                caption="**SpeedTest** completed in {} seconds".format(ms),
+                reply_to=reply_msg_id
+            )
+            await event.delete()
+    except Exception as exc:
+        await event.edit("""**SpeedTest** completed in {} seconds
+Download: {}
+Upload: {}
+Ping: {}
+
+__With the Following ERRORs__
+{}""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, str(exc)))
+
+
+def convert_from_bytes(size):
+    power = 2**10
+    n = 0
+    units = {
+        0: "",
+        1: "kilobytes",
+        2: "megabytes",
+        3: "gigabytes",
+        4: "terabytes"
+    }
+    while size > power:
+        size /= power
+        n += 1
+    return f"{round(size, 2)} {units[n]}"
+
