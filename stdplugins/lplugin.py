c42efbfd682e8c9f55f232f5e52c3fd19c8f17dc
Shrimadhav U K <scaleway@shrimadhavuk.me>
2019-03-03 14:48:01 +0000

    Fix spelling mistakes

--- stdplugins/lplugin.py
+++ stdplugins/lplugin.py
@@ -31,10 +31,16 @@ async def _(event):
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if event.reply_to_msg_id:
-        downloaded_file_name = await borg.download_media(
-            await event.get_reply_message(),
-            Config.TMP_DOWNLOAD_DIRECTORY
-        )
-        borg.load_plugin_from_file(downloaded_file_name)
+        try:
+            downloaded_file_name = await borg.download_media(
+                await event.get_reply_message(),
+                Config.TMP_DOWNLOAD_DIRECTORY
+            )
+            borg.load_plugin_from_file(downloaded_file_name)
+            await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
+        except Exception as e:
+            await event.edit(str(e))
+    if os.path.exists(downloaded_file_name):
         os.remove(downloaded_file_name)
-        await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
+
+
