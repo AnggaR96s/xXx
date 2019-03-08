9856f7aa6c58da4737be109af5005cd25ac9fb13
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-03-08 19:44:29 +0530

    Add !!ungban command

--- stdplugins/gban.py
+++ stdplugins/gban.py
@@ -15,3 +15,16 @@ async def _(event):
         )
     await event.delete()
 
+
+@borg.on(events.NewMessage(pattern=r"\!\!ungban ?(.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        await borg.send_message(
+            Config.G_BAN_LOGGER_GROUP,
+            "!ungban [user](tg://user?id={}) {}".format(r.from_id, reason)
+        )
+    await event.delete()
