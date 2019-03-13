04b8cf856a4515d9787ad384da150ccd67139f55
Shrimadhav U K <scaleway@shrimadhavuk.me>
2019-03-13 15:18:30 +0000

    [FIX] display progress

--- uniborg/util.py
+++ uniborg/util.py
@@ -33,7 +33,7 @@ async def is_read(borg, entity, message, is_out=None):
     return message_id <= max_id
 
 
-def progress(current, total, event, start, type_of_ps):
+async def progress(current, total, event, start, type_of_ps):
     now = time.time()
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
@@ -42,21 +42,14 @@ def progress(current, total, event, start, type_of_ps):
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
-
         progress = "[{0}{1}] \nPercent: {2}%\n".format(
             ''.join(["█" for i in range(math.floor(percentage / 5))]),
             ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + "{0} of {1}".format(
             humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),
-            # elapsed_time if elapsed_time != '' else "0 s",
-            estimated_total_time if estimated_total_time != '' else "0 s"
+            humanbytes(total)
         )
         await event.edit("{}\n {}".format(
             type_of_ps,
