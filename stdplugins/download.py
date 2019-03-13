1ea5f2bb0c3da22aaad0ae40beec646391f8eb71
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-03-13 21:21:43 +0530

    Add downlaods from URL

--- stdplugins/download.py
+++ stdplugins/download.py
@@ -1,4 +1,5 @@
 from telethon import events
+import aiohttp
 import asyncio
 import json
 import os
@@ -11,7 +12,7 @@ from telethon.tl.types import DocumentAttributeVideo
 from telethon.errors import MessageNotModifiedError
 
 import time
-from uniborg.util import progress
+from uniborg.util import progress, humanbytes, TimeFormatter
 
 
 @borg.on(events.NewMessage(pattern=r"\.download ?(.*)", outgoing=True))
@@ -35,5 +36,59 @@ async def _(event):
         end = datetime.now()
         ms = (end - start).seconds
         await event.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
+    elif input_str:
+        start = datetime.now()
+        url = input_str
+        file_name = os.path.basename(url)
+        to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
+        if "|" in input_str:
+            url, file_name = input_str.split("|")
+        downloaded_file_name = os.path.join(to_download_directory, file_name)
+        async with aiohttp.ClientSession() as session:
+            await download_coroutine(
+                session,
+                url,
+                downloaded_file_name,
+                event,
+                time.time()
+            )
+        end = datetime.now()
+        ms = (end - start).seconds
+        await event.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
     else:
         await event.edit("Reply to a message to download to my local server.")
+
+
+async def download_coroutine(session, url, file_name, event, start):
+    CHUNK_SIZE = 2341
+    downloaded = 0
+    async with session.get(url) as response:
+        print(response.headers)
+        total_length = response.headers["Content-Length"]
+        await event.edit("""Initiating Download
+URL: {}
+File Name: {}
+File Size: {}""".format(url, file_name, humanbytes(total_length)))
+        with open(file_name, "wb") as f_handle:
+            while True:
+                chunk = await response.content.read(CHUNK_SIZE)
+                if not chunk:
+                    break
+                f_handle.write(chunk)
+                downloaded += CHUNK_SIZE
+                now = time.time()
+                diff = now - start
+                if round(diff % 5.00) == 0 or downloaded == total_length:
+                    percentage = downloaded * 100 / total_length
+                    speed = downloaded / diff
+                    elapsed_time = round(diff) * 1000
+                    time_to_completion = round(
+                        (total_length - downloaded) / speed) * 1000
+                    estimated_total_time = elapsed_time + time_to_completion
+                    await event.edit("""Initiating Download
+URL: {}
+File Name: {}
+File Size: {}
+Downloaded: {}
+ETA: {}""".format(url, file_name, humanbytes(total_length), humanbytes(downloaded), TimeFormatter(estimated_total_time)))
+        return await response.release()
