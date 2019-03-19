b30360b8ef63f89b353684d3894087de1e928bb6
Shrimadhav U K <ScaleWay@shrimadhavuk.me>
2019-03-19 14:17:53 +0000

    Fix pin_message

--- stdplugins/pin_message.py
+++ stdplugins/pin_message.py
@@ -4,15 +4,21 @@ import inspect
 import os
 
 
-@borg.on(events.NewMessage(pattern=r"\.cpin", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.cpin ?(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
+    silent = True
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        silent = False
     if event.message.reply_to_msg_id is not None:
         message_id = event.message.reply_to_msg_id
         try:
-            await borg(f.channels.UpdatePinnedMessageRequest(event.chat_id, message_id, False))
+            await borg(f.messages.UpdatePinnedMessageRequest(event.chat_id, message_id, silent))
         except Exception as e:
             await event.edit(str(e))
+        else:
+            await event.delete()
     else:
         await event.edit("Reply to a message to pin the message in this Channel.")
