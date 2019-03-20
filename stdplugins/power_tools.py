fc1284dd4d543747193342c51317624fd0fff9b3
Shrimadhav U K <ScaleWay@shrimadhavuk.me>
2019-03-20 13:34:33 +0000

    Add restart and shutdown commands

---
+++ stdplugins/power_tools.py
@@ -0,0 +1,27 @@
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
+from telethon import events
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.restart", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Restarting ...\n`.ping` me or `.helpme` to check if I am online")
+    await borg.disconnect()
+    # https://archive.is/im3rt
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    # You probably don't need it but whatever
+    quit()
+
+
+@borg.on(events.NewMessage(pattern=r"\.shutdown", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Turning off ... Manually turn me on later")
+    await borg.disconnect()
+
