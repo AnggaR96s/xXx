e5e852700a323b369145036189c1f3214302b0f7
Shrimadhav U K <ScaleWay@shrimadhavuk.me>
2019-03-21 14:42:24 +0000

    Remove animations from restart

--- stdplugins/power_tools.py
+++ stdplugins/power_tools.py
@@ -6,6 +6,7 @@ Available Commands:
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
 # file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html
 from telethon import events
+import asyncio
 import os
 import sys
 
@@ -14,7 +15,12 @@ import sys
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("Restarting ...\n`.ping` me or `.helpme` to check if I am online")
+    # await asyncio.sleep(2)
+    # await event.edit("Restarting [██░] ...\n`.ping` me or `.helpme` to check if I am online")
+    # await asyncio.sleep(2)
+    # await event.edit("Restarting [███]...\n`.ping` me or `.helpme` to check if I am online")
+    # await asyncio.sleep(2)
+    await event.edit("Restarted. `.ping` me or `.helpme` to check if I am online")
     await borg.disconnect()
     # https://archive.is/im3rt
     os.execl(sys.executable, sys.executable, *sys.argv)
@@ -26,5 +32,5 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("Turning off ... Manually turn me on later")
+    await event.edit("Turning off ...Manually turn me on later")
     await borg.disconnect()
