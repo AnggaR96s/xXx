14b6ad31760007638cb1b0d90f5c779d62dc2871
Paul Larsen <pn1315@ic.ac.uk>
2019-03-23 13:01:55 +0530

    Allow pagination of the help module

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
+from math import ceil
 import re
 from telethon import events, custom
 
@@ -17,7 +18,8 @@ async def _(event):
         bot_results = await borg.inline_query(bot_username, search_query)
         i = 0
         for result in bot_results:
-            output_message += "{} {} `{}`\n\n".format(result.title, result.description, ".icb " + bot_username + " " + str(i + 1) + " " + search_query)
+            output_message += "{} {} `{}`\n\n".format(
+                result.title, result.description, ".icb " + bot_username + " " + str(i + 1) + " " + search_query)
             i = i + 1
         await event.edit(output_message)
     except Exception as e:
@@ -54,32 +56,19 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     "© @UniBorg",
                     text="Pong\n{}".format(ping_place_rs),
                     buttons=[
-                        [custom.Button.url("Join the Channel", "https://telegram.dog/UniBorg"), custom.Button.url("Join the Group", "https://telegram.dog/ShrimadhaVahdamirhS")],
-                        [custom.Button.url("Source Code", "https://GitLab.com/SpEcHiDe/UniBorg")]
+                        [custom.Button.url("Join the Channel", "https://telegram.dog/UniBorg"), custom.Button.url(
+                            "Join the Group", "https://telegram.dog/ShrimadhaVahdamirhS")],
+                        [custom.Button.url(
+                            "Source Code", "https://GitLab.com/SpEcHiDe/UniBorg")]
                     ],
                     link_preview=False
                 )
             elif "@UniBorg" in query:
-                buttons = []
-                same_row_buttons = []
-                i = 1
-                for plugin in borg._plugins:
-                    helpable_plugin = not plugin.startswith("_")
-                    if helpable_plugin:
-                        same_row_buttons.append(
-                            custom.Button.inline(
-                                "{} {}".format("✅", plugin),
-                                data="ub_plugin_{}".format(plugin)
-                            )
-                        )
-                    if (i % 3) == 0:
-                        buttons.append(same_row_buttons)
-                        same_row_buttons = []
-                    if helpable_plugin:
-                        i = i + 1
+                buttons = paginate_help(0, borg._plugins, "helpme")
                 result = builder.article(
                     "© @UniBorg",
-                    text="{}\nCurrently Loaded Plugins: {}".format(query, len(borg._plugins)),
+                    text="{}\nCurrently Loaded Plugins: {}".format(
+                        query, len(borg._plugins)),
                     buttons=buttons,
                     link_preview=False
                 )
@@ -88,8 +77,10 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     "© @UniBorg",
                     text=query,
                     buttons=[
-                        [custom.Button.url("Join the Channel", "https://telegram.dog/UniBorg"), custom.Button.url("Join the Group", "https://telegram.dog/ShrimadhaVahdamirhS")],
-                        [custom.Button.url("Source Code", "https://GitLab.com/SpEcHiDe/UniBorg")]
+                        [custom.Button.url("Join the Channel", "https://telegram.dog/UniBorg"), custom.Button.url(
+                            "Join the Group", "https://t.me/joinchat/AHAujEjG4FBO-TH-NrVVbg")],
+                        [custom.Button.url(
+                            "Source Code", "https://GitHub.com/SpEcHiDe/UniBorg")]
                     ],
                     link_preview=False
                 )
@@ -99,20 +90,64 @@ if Config.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 text="""Try @UniBorg
 You can log-in as Bot or User and do many cool things with your Telegram account.
 
-All instaructions to run @UniBorg in your PC has been explained in https://t.me/UniBorg/4""",
+All instaructions to run @UniBorg in your PC has been explained in https://github.com/SpEcHiDe/UniBorg""",
                 buttons=[
-                    [custom.Button.url("Join the Channel", "https://telegram.dog/UniBorg"), custom.Button.url("Join the Group", "https://telegram.dog/ShrimadhaVahdamirhS")],
-                    [custom.Button.url("Source Code", "https://GitLab.com/SpEcHiDe/UniBorg")]
+                    [custom.Button.url("Join the Channel", "https://telegram.dog/UniBorg"), custom.Button.url(
+                        "Join the Group", "https://t.me/joinchat/AHAujEjG4FBO-TH-NrVVbg")],
+                    [custom.Button.url(
+                        "Source Code", "https://GitHub.com/SpEcHiDe/UniBorg")]
                 ],
                 link_preview=False
             )
         await event.answer([result] if result else None)
 
 
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"helpme_next\((.+?)\)")))
+    async def on_plug_in_callback_query_handler(event):
+        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+        buttons = paginate_help(current_page_number + 1, borg._plugins, "helpme")
+        logger.info(event.stringify())
+        await event.edit(buttons=buttons)
+
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"helpme_prev\((.+?)\)")))
+    async def on_plug_in_callback_query_handler(event):
+        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+        buttons = paginate_help(current_page_number - 1, borg._plugins, "helpme")
+        logger.info(event.stringify())
+        await event.edit(buttons=buttons)
+
+
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"ub_plugin_(.*)")))
     async def on_plug_in_callback_query_handler(event):
         plugin_name = event.data_match.group(1).decode("UTF-8")
         help_string = borg._plugins[plugin_name].__doc__
-        reply_pop_up_alert = help_string if help_string is not None else "No DOCSTRING has been setup for {} plugin".format(plugin_name)
-        reply_pop_up_alert += "\n\n Use .unload {} to remove this plugin from the loaded plugins\n© @UniBorg".format(plugin_name)
+        reply_pop_up_alert = help_string if help_string is not None else "No DOCSTRING has been setup for {} plugin".format(
+            plugin_name)
+        reply_pop_up_alert += "\n\n Use .unload {} to remove this plugin from the loaded plugins\n© @UniBorg".format(
+            plugin_name)
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+
+def paginate_help(page_number, loaded_plugins, prefix):
+    helpable_plugins = []
+    for p in loaded_plugins:
+        if not p.startswith("_"):
+            helpable_plugins.append(p)
+    helpable_plugins = sorted(helpable_plugins)
+    modules = [custom.Button.inline(
+        "{} {}".format("✅", x),
+        data="ub_plugin_{}".format(x))
+        for x in helpable_plugins]
+    pairs = list(zip(modules[::2], modules[1::2]))
+    if len(modules) % 2 == 1:
+        pairs.append((modules[-1],))
+    max_num_pages = ceil(len(pairs) / 7)
+    modulo_page = page_number % max_num_pages
+    if len(pairs) > 7:
+        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + \
+                [
+                    (custom.Button.inline("Previous", data="{}_prev({})".format(prefix, modulo_page)),
+                    custom.Button.inline("Next", data="{}_next({})".format(prefix, modulo_page)))
+                ]
+    return pairs
