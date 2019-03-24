9c7637344168f6b373f9a537df75db721342c5b7
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-03-24 10:52:11 +0530

    get_id without being in the chat (unless not banned)

--- stdplugins/get_id.py
+++ stdplugins/get_id.py
@@ -4,18 +4,27 @@ from telethon import events
 from telethon.utils import pack_bot_file_id
 
 
-@borg.on(events.NewMessage(pattern=r"\.get_id", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.get_id ?(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        try:
+            chat = await borg.get_entity(input_str)
+        except Exception as e:
+            await event.edit(str(e))
+            return None
+        else:
+            a_msg = "{} chat ID: `{}`\n".format(input_str, "-100" + str(chat.id))
     if event.reply_to_msg_id:
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("The BOT API file ID of this media is `{}`".format(str(bot_api_file_id)))
+            await event.edit("{}The BOT API file ID of this media is `{}`".format(a_msg, str(bot_api_file_id)))
         else:
             chat = await event.get_input_chat()
-            await event.edit("The current chat's ID is `{}` and the replied user as an ID: `{}`".format(str(event.chat_id), str(r_msg.from_id)))
+            await event.edit("{}The current chat's ID is `{}` and the replied user as an ID: `{}`".format(a_msg, str(event.chat_id), str(r_msg.from_id)))
     else:
         chat = await event.get_input_chat()
-        await event.edit("The current chat's ID is `{}`!".format(str(event.chat_id)))
+        await event.edit("{}The current chat's ID is `{}`!".format(a_msg, str(event.chat_id)))
