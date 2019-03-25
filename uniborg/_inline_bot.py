2fcf126998cf1dde3af5b8a0fe9697760d1c0124
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-03-25 21:39:03 +0530

    Allow only self to tap on the Previous and Next buttons

--- uniborg/_inline_bot.py
+++ uniborg/_inline_bot.py
@@ -104,20 +104,28 @@ All instaructions to run @UniBorg in your PC has been explained in https://githu
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"helpme_next\((.+?)\)")))
     async def on_plug_in_callback_query_handler(event):
-        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
-        buttons = paginate_help(current_page_number + 1, borg._plugins, "helpme")
-        # logger.info(event.stringify())
-        # https://t.me/TelethonChat/115200
-        await event.edit(buttons=buttons)
+        if event.query.user_id == borg.uid:
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+            buttons = paginate_help(current_page_number + 1, borg._plugins, "helpme")
+            # logger.info(event.stringify())
+            # https://t.me/TelethonChat/115200
+            await event.edit(buttons=buttons)
+        else:
+            reply_pop_up_alert = "Please get your own @UniBorg, and don't edit my messages!"
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"helpme_prev\((.+?)\)")))
     async def on_plug_in_callback_query_handler(event):
-        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
-        buttons = paginate_help(current_page_number - 1, borg._plugins, "helpme")
-        # logger.info(event.stringify())
-        # https://t.me/TelethonChat/115200
-        await event.edit(buttons=buttons)
+        if event.query.user_id == borg.uid:
+            current_page_number = int(event.data_match.group(1).decode("UTF-8"))
+            buttons = paginate_help(current_page_number - 1, borg._plugins, "helpme")
+            # logger.info(event.stringify())
+            # https://t.me/TelethonChat/115200
+            await event.edit(buttons=buttons)
+        else:
+            reply_pop_up_alert = "Please get your own @UniBorg, and don't edit my messages!"
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"ub_plugin_(.*)")))
@@ -132,6 +140,7 @@ All instaructions to run @UniBorg in your PC has been explained in https://githu
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
+    number_of_rows = 5
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
@@ -144,10 +153,10 @@ def paginate_help(page_number, loaded_plugins, prefix):
     pairs = list(zip(modules[::2], modules[1::2]))
     if len(modules) % 2 == 1:
         pairs.append((modules[-1],))
-    max_num_pages = ceil(len(pairs) / 7)
+    max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + \
+    if len(pairs) > number_of_rows:
+        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
                 [
                     (custom.Button.inline("Previous", data="{}_prev({})".format(prefix, modulo_page)),
                     custom.Button.inline("Next", data="{}_next({})".format(prefix, modulo_page)))
