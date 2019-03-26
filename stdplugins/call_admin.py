36453217ea0cfd3c9ceb39e93ff5227b900b89eb
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-03-26 19:56:25 +0530

    @admin tag will now work without @MaterTagAlertBot

--- stdplugins/call_admin.py
+++ stdplugins/call_admin.py
@@ -1,11 +1,15 @@
-"""/admin Plugin for @UniBorg"""
-from telethon import events
+""".admin Plugin for @UniBorg"""
 import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
 
-@borg.on(events.NewMessage(pattern=r"\/admin", outgoing=True))
+
+@borg.on(events.NewMessage(pattern=r"\@admin", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("@admin")
-    await asyncio.sleep(5)
-    await event.delete()
+    mentions = "admin"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    await event.edit(mentions)
