29902ea177fd0a1821fc24c8cfaa87a71c73a26e
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-04-01 19:55:37 +0530

    Option to choose the type of output for speedtest. [Default is image]

--- stdplugins/speedtest.py
+++ stdplugins/speedtest.py
@@ -1,15 +1,25 @@
 """Check your internet speed powered by speedtest.net
-Syntax: .speedtest"""
+Syntax: .speedtest
+Available Options: image, file, text"""
 from telethon import events
 from datetime import datetime
 import io
 import speedtest
 
 
-@borg.on(events.NewMessage(pattern=r"\.speedtest", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.speedtest ?(.*)", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
+    input_str = event.pattern_match.group(1)
+    as_text = False
+    as_document = True
+    if input_str == "image":
+        as_document = False
+    elif input_str == "file":
+        as_document = True
+    elif input_str == "text":
+        as_text = True
     await event.edit("Calculating my internet speed. Please wait!")
     start = datetime.now()
     s = speedtest.Speedtest()
@@ -22,20 +32,32 @@ async def _(event):
     download_speed = response.get("download")
     upload_speed = response.get("upload")
     ping_time = response.get("ping")
+    client_infos = response.get("client")
+    i_s_p = client_infos.get("isp")
+    i_s_p_rating = client_infos.get("isprating")
     reply_msg_id = event.message.id
     if event.reply_to_msg_id:
         reply_msg_id = event.reply_to_msg_id
     try:
         response = s.results.share()
         speedtest_image = response
-        await borg.send_file(
-            event.chat_id,
-            speedtest_image,
-            caption="**SpeedTest** completed in {} seconds".format(ms),
-            force_document=True,
-            reply_to=reply_msg_id
-        )
-        await event.delete()
+        if as_text:
+            await event.edit("""**SpeedTest** completed in {} seconds
+Download: {}
+Upload: {}
+Ping: {}
+Internet Service Provider: {}
+ISP Rating: {}""".format(ms, convert_from_bytes(download_speed), convert_from_bytes(upload_speed), ping_time, i_s_p, i_s_p_rating))
+        else:
+            await borg.send_file(
+                event.chat_id,
+                speedtest_image,
+                caption="**SpeedTest** completed in {} seconds".format(ms),
+                force_document=as_document,
+                reply_to=reply_msg_id,
+                allow_cache=False
+            )
+            await event.delete()
     except Exception as exc:
         await event.edit("""**SpeedTest** completed in {} seconds
 Download: {}
