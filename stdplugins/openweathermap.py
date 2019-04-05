711a92bca365b56622a58c7c45af0f581579707c
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-04-05 19:17:50 +0530

    Fix weather. Display all relevant details that the API returns

--- stdplugins/openweathermap.py
+++ stdplugins/openweathermap.py
@@ -1,7 +1,9 @@
 """Get weather data using OpenWeatherMap
 Syntax: .weather <Location> """
-from telethon import events
+
 import requests
+import time
+from telethon import events
 
 
 @borg.on(events.NewMessage(pattern=r"\.weather (.*)", outgoing=True))
@@ -12,6 +14,21 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     response_api = requests.get(sample_url.format(input_str, Config.OPEN_WEATHER_MAP_APPID)).json()
     if response_api["cod"] == 200:
-        await event.edit("The temperature is around {}°C, in {}.".format(response_api["main"]["temp"], input_str))
+        await event.edit(
+            """{}
+{}°С temperature from {} to {}°С, wind {}m/s.clouds {}%, {}hpa
+Sunrise: {}
+Sunset: {}""".format(
+                input_str,
+                response_api["main"]["temp"],
+                response_api["main"]["temp_min"],
+                response_api["main"]["temp_max"],
+                response_api["wind"]["speed"],
+                response_api["clouds"]["all"],
+                response_api["main"]["pressure"],
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(response_api["sys"]["sunrise"])),
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(response_api["sys"]["sunset"]))
+            )
+        )
     else:
         await event.edit(response_api["message"])
