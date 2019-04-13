56efb3a958d3ebb7319ae02528f5e03cc1835ddc
Ravana <47528708+ravana69@users.noreply.github.com>
2019-04-13 23:10:57 +0530

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -130,10 +130,10 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"**😄 I'm afk since {afk_since} " + \
-            f"and I will be back soon 😄\n__Reason:__ {reason}" \
+        message_to_reply = f"`😄 I'm afk since` {afk_since} " + \
+            f"`and I will be back soon 😄`\n__Reason:__ {reason}" \
             if reason \
-            else f"**😄 I'm afk since {afk_since} and I will be back soon 😄**"
+            else f"`😄 I'm afk since` {afk_since} `and I will be back soon 😄`"
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in borg.storage.last_afk_message:  # pylint:disable=E0602
