c90cc6557b981abfcbc84e4716023ca565f8e7c9
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-04-14 14:02:53 +0530

    Properly **TAG** admins with mention notification without @MasterTagAlertBot

--- stdplugins/call_admin.py
+++ stdplugins/call_admin.py
@@ -4,12 +4,19 @@ from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
-@borg.on(events.NewMessage(pattern=r"\@admin", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.admin", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "admin"
+    mentions = "@admin: **Spam Spotted**"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f"[\u2063](tg://user?id={x.id})"
-    await event.edit(mentions)
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
