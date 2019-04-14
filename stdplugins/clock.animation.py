f34a177c05daab405ebd3167c1b0920d068f88a6
Ravana <47528708+ravana69@users.noreply.github.com>
2019-04-14 19:13:49 +0530

    Clock trigger changed

--- stdplugins/clock.animation.py
+++ stdplugins/clock.animation.py
@@ -6,7 +6,7 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.clock animation", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.clock", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
