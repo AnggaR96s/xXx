3d4a50263e118b14301ecb760094c99eab4f10c9
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-04-16 22:30:05 +0530

    Add  to config.py

--- sample_config.py
+++ sample_config.py
@@ -90,6 +90,9 @@ class Config(object):
     DB_URI = os.environ.get("DATABASE_URL", None)
     # number of rows of buttons to be displayed in .helpme command
     NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD", 5))
+    # specify command handler that should be used for the plugins
+    # this should be a valid "regex" pattern
+    COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "^\.")
 
 class Production(Config):
     LOGGER = False
