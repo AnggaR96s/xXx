9ebe96b628abb666b42880633903eaf414cf48b5
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-04-16 18:35:58 +0530

    LINT: telesetup

--- telesetup.py
+++ telesetup.py
@@ -5,28 +5,27 @@ import telethon.sync
 from telethon import TelegramClient, events
 from telethon.tl import types, functions
 import os
+from config import Development as Config
 
-APP_ID = os.environ.get("APP_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
 
 # https://t.me/TelethonChat/13265
-client = TelegramClient(input("Enter your username: @"), APP_ID, API_HASH).start()
+client = TelegramClient(input("Enter your username: @"), Config.APP_ID, Config.API_HASH).start()
 
 
 def progress(current, total):
   print("Downloaded: " + str(current) + " of " + str(total) + " Percent: " + str((current / total) * 100))
 
+
 spechide = client.get_me()
 print(spechide.stringify())
 # client.send_message(spechide, "Dummy Message to get active session")
 
+
 """Interactive client to test various things
 """
-
 if __name__ == "__main__":
   """@client.on(events.NewMessage)
   def myeventhandler(event):
     print(event.raw_text)"""
   print("Loaded")
   # client.run_until_disconnected()
-
