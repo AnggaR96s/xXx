58ee2d474d375726c00d3acf23e4d11ef29ae184
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-04-18 14:16:31 +0530

    Allow optional parameter to allow SUDO access to a plugin, defaults to False

--- uniborg/util.py
+++ uniborg/util.py
@@ -19,10 +19,11 @@ else:
         from config import Development as Config
 
 
-def admin_cmd(pattern=None, **args):
+def admin_cmd(pattern=None, allow_sudo=False, **args):
     if pattern is not None:
         args["pattern"] = re.compile(Config.COMMAND_HAND_LER + pattern)
-    args["from_users"] = list(Config.SUDO_USERS)
+    if allow_sudo:
+        args["from_users"] = list(Config.SUDO_USERS)
     return events.NewMessage(**args)
 
 
