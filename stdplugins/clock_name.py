7b942201e3904ab35813b46039bad9f0346f62e6
parambika <49111944+parambika@users.noreply.github.com>
2019-04-25 22:13:35 +0530

    Update clock_name.py

--- stdplugins/clock_name.py
+++ stdplugins/clock_name.py
@@ -19,7 +19,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M")
-        name = f"{HM} - {DMY}"
+        name = f"{HM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
