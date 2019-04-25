206ef8cfdd68eb6606c4f93da50cf935c8cb8cf0
parambika <49111944+parambika@users.noreply.github.com>
2019-04-25 21:01:36 +0530

    Add files via upload

---
+++ stdplugins/clock_name.py
@@ -0,0 +1,37 @@
+# For @UniBorg
+# (c) Neel Pragmatist
+"""Auto Profile Updation Commands
+.autopp"""
+import asyncio
+import time
+from telethon.tl import functions
+from telethon.errors import FloodWaitError
+from uniborg.util import admin_cmd
+
+
+DEL_TIME_OUT = 70
+
+
+@borg.on(admin_cmd("clockname"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    while True:
+        DMY = time.strftime("%d.%m.%Y")
+        HM = time.strftime("%H:%M:%S")
+        name = f"⌚️ {HM}"
+        logger.info(name)
+        try:
+            await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                last_name = name
+            ))
+        except FloodWaitError as ex:
+            logger.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+        # else:
+            # logger.info(r.stringify())
+            # await borg.send_message(  # pylint:disable=E0602
+            #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+            #     "Changed Profile Picture"
+            # )
+        await asyncio.sleep(DEL_TIME_OUT)
