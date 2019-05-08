4644f6ed3fe8e2f165febacfd7d9d153c33c5b6c
parambika <49111944+parambika@users.noreply.github.com>
2019-05-08 14:40:42 +0530

    Update ping.py

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -8,7 +8,7 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit("Pong!")
+    await event.edit("Ding!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit("`Ding!\n{}ms`".format(ms))
