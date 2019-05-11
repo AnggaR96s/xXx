49960aa5e0061d13b39fbe0241b9e173fa5fd4aa
parambika <49111944+parambika@users.noreply.github.com>
2019-05-11 16:27:26 +0530

    Update afk.py

--- stdplugins/afk.py
+++ stdplugins/afk.py
@@ -138,7 +138,7 @@ async def on_afk(event):
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
         message_to_reply = f"I'm afk since {afk_since} " + \
-            f"and I will be back soon\n__Reason:__ {reason}" \
+            f"and I will be back soon\n**Reason:** {reason}" \
             if reason \
             else f"I'm afk since {afk_since} and I will be back soon."
         msg = await event.reply(message_to_reply)
