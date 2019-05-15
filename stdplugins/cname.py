96c373551b60f9b81fbbdebb62daff477ea9182c
parambika <49111944+parambika@users.noreply.github.com>
2019-05-15 19:22:43 +0530

    Update cname.py

--- stdplugins/cname.py
+++ stdplugins/cname.py
@@ -19,7 +19,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m")
         HM = time.strftime("%H:%M")
-        name = f"{HM}|🙏 śrīṁ16 🙏|{DMY}"
+        name = f"{HM}|🕉|{DMY}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
