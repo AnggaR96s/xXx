ee865f9cf6280df43143918e0c6c8b5810110b67
parambika <49111944+parambika@users.noreply.github.com>
2019-05-21 19:30:30 +0530

    Update ping.py

--- stdplugins/ping.py
+++ stdplugins/ping.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("ping"))
+@borg.on(admin_cmd("om"))
 async def _(event):
     if event.fwd_from:
         return
