3c77b9eccc04b48c33f4c8cd0327623add613685
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-05-25 14:39:21 +0530

    beautify get_id

--- stdplugins/get_id.py
+++ stdplugins/get_id.py
@@ -5,28 +5,17 @@ from telethon.utils import pack_bot_file_id
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("get_id ?(.*)"))
+@borg.on(admin_cmd("get_id"))
 async def _(event):
     if event.fwd_from:
         return
-    input_str = event.pattern_match.group(1)
-    a_msg = ""
-    if input_str:
-        try:
-            chat = await borg.get_entity(input_str)
-        except Exception as e:
-            await event.edit(str(e))
-            return None
-        else:
-            a_msg = "{} chat ID: `{}`\n".format(input_str, "-100" + str(chat.id))
     if event.reply_to_msg_id:
+        chat = await event.get_input_chat()
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("{}The BOT API file ID of this media is `{}`".format(a_msg, str(bot_api_file_id)))
+            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
         else:
-            chat = await event.get_input_chat()
-            await event.edit("{}The current chat's ID is `{}` and the replied user as an ID: `{}`".format(a_msg, str(event.chat_id), str(r_msg.from_id)))
+            await event.edit("Current Chat ID: `{}`\nFrom User ID: `{}`".format(str(event.chat_id), str(r_msg.from_id)))
     else:
-        chat = await event.get_input_chat()
-        await event.edit("{}The current chat's ID is `{}`!".format(a_msg, str(event.chat_id)))
+        await event.edit("Current Chat ID: `{}`".format(str(event.chat_id)))
