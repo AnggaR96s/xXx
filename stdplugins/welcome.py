b9d03acd7a7210fb745b3d9dbfa121221df10853
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-06-28 22:11:39 +0530

    allow media messages in welcome note

--- stdplugins/welcome.py
+++ stdplugins/welcome.py
@@ -4,6 +4,7 @@ Commands:
 .savewelcome <Welcome Message>"""
 
 from telethon import events
+from telethon.utils import pack_bot_file_id
 from sql_helpers.welcome_sql import get_current_welcome_settings, \
     add_welcome_setting, rm_welcome_setting, update_previous_welcome
 from uniborg.util import admin_cmd
@@ -31,7 +32,8 @@ async def _(event):
             current_saved_welcome_message = cws.custom_welcome_message
             mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
             current_message = await event.reply(
-                current_saved_welcome_message.format(mention=mention)
+                current_saved_welcome_message.format(mention=mention),
+                file=cws.media_file_id
             )
             update_previous_welcome(event.chat_id, current_message.id)
 
@@ -40,10 +42,15 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    input_str = event.pattern_match.group(1)
-    add_welcome_setting(event.chat_id, input_str, True, 0)
-    await event.edit("Welcome note saved. ")
-
+    msg = await event.get_reply_message()
+    if msg and msg.media:
+        bot_api_file_id = pack_bot_file_id(msg.media)
+        add_welcome_setting(event.chat_id, msg.message, True, 0, bot_api_file_id)
+        await event.edit("Welcome note saved. ")
+    else:
+        input_str = event.text.split(None, 1)
+        add_welcome_setting(event.chat_id, input_str[1], True, 0)
+        await event.edit("Welcome note saved. ")
 
 @borg.on(admin_cmd("clearwelcome"))  # pylint:disable=E0602
 async def _(event):
