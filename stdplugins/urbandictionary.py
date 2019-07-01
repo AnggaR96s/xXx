c5ff7771bad056baca067d0695abaed01c0f27a4
Angga R Saputra <superuserxyyx@gmail.com>
2019-07-02 02:47:19 +0700

    Update urbandictionary.py

--- stdplugins/urbandictionary.py
+++ stdplugins/urbandictionary.py
@@ -2,13 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Urban Dictionary
-Syntax: .ud Query"""
+Syntax: .urban Query"""
 from telethon import events
 import urbandict
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("ud (.*)"))
+@borg.on(admin_cmd("urban (.*)"))
 async def _(event):
     if event.fwd_from:
         return
