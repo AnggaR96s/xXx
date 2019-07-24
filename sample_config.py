5072a2b93eed6d40c39750f87aa2c1a6fdd04903
Shrimadhav U K <ScaleWay@shrimadhavuk.me>
2019-07-24 19:27:41 +0530

    Fix bad SED bots. Ref: https://t.me/MarieNews/60

--- sample_config.py
+++ sample_config.py
@@ -100,6 +100,8 @@ class Config(object):
     G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
     #
     TELE_GRAM_2FA_CODE = os.environ.get("TELE_GRAM_2FA_CODE", None)
+    #
+    GROUP_REG_SED_EX_BOT_S = os.environ.get("GROUP_REG_SED_EX_BOT_S", r"(regex|moku|BananaButler_|rgx|l4mR)bot")
 
 
 class Production(Config):
