318dd579c8b6a3b25e985f4361ed8f095b04a776
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-07-27 11:39:51 +0530

    Add Google Drive support

--- stdplugins/rapidleech.py
+++ stdplugins/rapidleech.py
@@ -35,14 +35,16 @@ async def _(event):
         list_of_urls = get_inner_text(
             reply_message.message, reply_message.entities)
     converted_links = ""
-    for a_url in list_of_urls:
-        converted_link_infos = await get_direct_ip_specific_link(a_url)
-        if "url" in converted_link_infos:
-            converted_link = converted_link_infos["url"]
-            converted_links += f"[{a_url}]({converted_link}) \n\n"
-        elif "err" in converted_link_infos:
-            err = converted_link_infos["err"]
-            converted_links += f"`{a_url}` returned `{err}`\n\n"
+    if len(list_of_urls) > 0:
+        converted_links += "Trying to generate IP specific link\n\nഞെക്കി പിടി \n"
+        for a_url in list_of_urls:
+            converted_link_infos = await get_direct_ip_specific_link(a_url)
+            if "url" in converted_link_infos:
+                converted_link = converted_link_infos["url"]
+                converted_links += f"[{a_url}]({converted_link}) \n\n"
+            elif "err" in converted_link_infos:
+                err = converted_link_infos["err"]
+                converted_links += f"`{a_url}` returned `{err}`\n\n"
     await event.reply(converted_links)
 
 
@@ -51,6 +53,9 @@ async def get_direct_ip_specific_link(link: str):
     OPEN_LOAD_DOMAINS = r"(?:openload\.(?:co|io|link|pw)|oload\.(?:tv|biz|stream|site|xyz|win|download|cloud|cc|icu|fun|club|info|press|pw|life|live|space|services|website)|oladblock\.(?:services|xyz|me)|openloed\.co)"
     OPEN_LOAD_VALID_URL = r"(?x)https?://(?P<host>(?:www\.)?%s)/(?:f|embed)/(?P<id>[a-zA-Z0-9-_]+)" % OPEN_LOAD_DOMAINS
     # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/openload.py#L246-L255
+    # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/googledrive.py#L16-L27
+    GOOGLE_DRIVE_VALID_URLS = r"(?x)https?://(?:(?:docs|drive)\.google\.com/(?:(?:uc|open)\?.*?id=|file/d/)|video\.google\.com/get_player\?.*?docid=)(?P<id>[a-zA-Z0-9_-]{28,})"
+    # https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/extractor/googledrive.py#L16-L27
     dl_url = None
     if "zippyshare.com" in link:
         async with aiohttp.ClientSession() as session:
@@ -112,6 +117,44 @@ async def get_direct_ip_specific_link(link: str):
                     "err": http_response_text
                 }
         # https://stackoverflow.com/a/47726003/4723940
+    elif re.search(GOOGLE_DRIVE_VALID_URLS, link):
+        file_id = re.search(GOOGLE_DRIVE_VALID_URLS, link).group("id")
+        async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar()) as session:
+            step_zero_url = "https://drive.google.com/uc?export=download&id={}".format(file_id)
+            http_response = await session.get(step_zero_url, allow_redirects=False)
+            if "location" in http_response.headers:
+                # in case of small file size, Google downloads directly
+                file_url = http_response.headers["location"]
+                if "accounts.google.com" in file_url:
+                    dl_url = {
+                        "err": "Private Google Drive URL"
+                    }
+                else:
+                    dl_url = {
+                        "url": file_url
+                    }
+            else:
+                # in case of download warning page
+                http_response_text = await http_response.text()
+                response_b_soup = BeautifulSoup(http_response_text, "html.parser")
+                warning_page_url = "https://drive.google.com" + response_b_soup.find("a", {"id": "uc-download-link"}).get("href")
+                file_name_and_size = response_b_soup.find("span", {"class": "uc-name-size"}).text
+                http_response_two = await session.get(warning_page_url, allow_redirects=False)
+                if "location" in http_response_two.headers:
+                    file_url = http_response_two.headers["location"]
+                    if "accounts.google.com" in file_url:
+                        dl_url = {
+                            "err": "Private Google Drive URL"
+                        }
+                    else:
+                        dl_url = {
+                            "url": file_url,
+                            "name": file_name_and_size
+                        }
+                else:
+                    dl_url = {
+                        "err": "Unsupported Google Drive URL"
+                    }
     else:
         dl_url = {
             "err": "Unsupported URL"
