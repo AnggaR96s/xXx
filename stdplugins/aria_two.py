fab6ca4adc2cdf8fa6cbd0a21aa11f98f3172b5b
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-07-28 13:52:55 +0530

    re-write using admin_cmd

--- stdplugins/aria_two.py
+++ stdplugins/aria_two.py
@@ -12,133 +12,114 @@ By:- @Zero_cool7870
 
 Credits: https://github.com/jaskaranSM/UniBorg/blob/b42cd70144143ce079e5fb3aed49c9aa1412481b/stdplugins/aria.py
 """
+
 import aria2p
-from telethon import events
 import asyncio
+import io
 import os
+from uniborg.util import admin_cmd
 
-
-cmd = "aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true"
+ARIA_2_PORT = 6800
+cmd = f"aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port {ARIA_2_PORT}  --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true"
 
 aria2_is_running = os.system(cmd)
 
 aria2 = aria2p.API(
     aria2p.Client(
         host="http://localhost",
-        port=6800,
+        port=ARIA_2_PORT,
         secret=""
     )
 )
 
 
-@borg.on(events.NewMessage(pattern=r"\.magnet", outgoing=True))
+@borg.on(admin_cmd("addmagnet"))
 async def magnet_download(event):
     if event.fwd_from:
         return
-    var = event.text
-    var = var[8:]
-
-    magnet_uri = var
-    magnet_uri = magnet_uri.replace("`", "")
-    print(magnet_uri)
-
+    var = event.raw_text
+    var = var.split(" ")
+    magnet_uri = var[1]
+    logger.info(magnet_uri)
     # Add Magnet URI Into Queue
     try:
         download = aria2.add_magnet(magnet_uri)
-    except:
-        await event.edit("`Error: Make Sure Magnet link is correct.`")
+    except Exception as e:
+        await event.edit("**Error**: __Make Sure Magnet link is correct.__\n`{}`".format(str(e)))
         return
-
-    await event.edit("`Downloading From Magnet Link: `\n\n" + magnet_uri + "\nType show to check status")
+    m = await event.reply("Downloading From Magnet Link:\nType `showariastatus` to check status")
     await asyncio.sleep(5)
-    await event.delete()
+    await m.delete()
 
 
-@borg.on(events.NewMessage(pattern=r"\.tor", outgoing=True))
+@borg.on(admin_cmd("addtorrent"))
 async def torrent_download(event):
     if event.fwd_from:
         return
-
-    var = event.text[5:]
-
-    torrent_file_path = var
-    torrent_file_path = torrent_file_path.replace("`", "")
-    print(torrent_file_path)
-
+    var = event.raw_text
+    var = var.split(" ")
+    torrent_file_path = var[1]
+    logger.info(torrent_file_path)
     # Add Torrent Into Queue
     try:
         download = aria2.add_torrent(
             torrent_file_path, uris=None, options=None, position=None)
-    except:
-        await event.edit("`Torrent File Not Found...`")
+    except Exception as e:
+        await event.edit("**Error**: __Make Sure Torrent PATH is correct.__\n`{}`".format(str(e)))
         return
-
-    await event.edit("`Downloading From Torrent File: `\n\n" + torrent_file_path + "\nType show to check status")
+    m = await event.reply("Downloading From given Link:\nType `showariastatus` to check status")
     await asyncio.sleep(5)
-    await event.delete()
+    await m.delete()
 
 
-@borg.on(events.NewMessage(pattern=r"\.ariaRM", outgoing=True))
+@borg.on(admin_cmd("ariaRM"))
 async def remove_all(event):
     if event.fwd_from:
         return
-
     removed = aria2.remove_all()
-
     if removed == False:  # If API returns False Try to Remove Through System Call.
         os.system("aria2p remove-all")
-
     await event.edit("`Removed All Downloads.`")
 
 
-@borg.on(events.NewMessage(pattern=r"\.ariaP", outgoing=True))
+@borg.on(admin_cmd("ariaP"))
 async def pause_all(event):
     if event.fwd_from:
         return
     # Pause ALL Currently Running Downloads.
     paused = aria2.pause_all(force=True)
-
     await event.edit("Output: " + str(paused))
 
 
-@borg.on(events.NewMessage(pattern=r"\.ariaResume", outgoing=True))
+@borg.on(admin_cmd("ariaResume"))
 async def resume_all(event):
     if event.fwd_from:
         return
-
     resumed = aria2.resume_all()
-
     await event.edit("Output: " + str(resumed))
 
 
-@borg.on(events.NewMessage(pattern=r"\.show", outgoing=True))
+@borg.on(admin_cmd("showariastatus"))
 async def show_all(event):
     if event.fwd_from:
         return
-    output = "output.txt"
     # Show All Downloads
     downloads = aria2.get_downloads()
-
     msg = ""
-
     for download in downloads:
         msg = msg + "File: " + str(download.name) + "\nSpeed: " + str(download.download_speed_string()) + "\n" + \
             "Progress: " + str(download.progress_string()) + \
             "\nETA:  " + str(download.eta_string()) + "\n\n"
-    print(msg)
-    if len(msg) <= 4096:
+    if len(msg) <= Config.MAX_MESSAGE_SIZE_LIMIT:
         await event.edit("`Current Downloads: `\n" + msg)
     else:
-        await event.edit("`Output is huge. Sending as a file...`")
-        with open(output, 'w') as f:
-            f.write(msg)
-        await asyncio.sleep(2)
-        await event.delete()
-        await borg.send_file(
-            event.chat_id,
-            output,
-            force_document=True,
-            supports_streaming=False,
-            allow_cache=False,
-            reply_to=event.message.id,
-        )
+        with io.BytesIO(str.encode(msg)) as out_file:
+            out_file.name = "ariastatus.txt"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="`Output is huge. Sending as a file...`"
+            )
+            await event.delete()
