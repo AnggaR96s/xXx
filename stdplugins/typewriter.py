72a18b6fac22d79d1731892a5da14ebf37e83c71
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-08-25 09:15:38 +0530

    re-introduce typewriter

--- stdplugins/typewriter.py
+++ stdplugins/typewriter.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" (c) Shrimadhav U K
-.ty text"""
-from telethon import events
+# (c) Shrimadhav U K
 import asyncio
+from uniborg.util import admin_cmd
 
 
-@borg.on(events.NewMessage(pattern=r"\.ty (.*)", outgoing=True))
+@borg.on(admin_cmd(pattern="type (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     # https://t.me/AnotherGroup/176551
     input_str = event.pattern_match.group(1)
+    shiiinabot = "\u2060"
+    for i in range(601):
+        shiiinabot += "\u2060"
+    try:
+        await event.edit(shiiinabot)
+    except Exception as e:
+        logger.warn(str(e))
     typing_symbol = "|"
     DELAY_BETWEEN_EDITS = 0.3
     previous_text = ""
@@ -20,7 +26,15 @@ async def _(event):
     for character in input_str:
         previous_text = previous_text + "" + character
         typing_text = previous_text + "" + typing_symbol
-        await event.edit(typing_text)
+        try:
+            await event.edit(typing_text)
+        except Exception as e:
+            logger.warn(str(e))
+            pass
         await asyncio.sleep(DELAY_BETWEEN_EDITS)
-        await event.edit(previous_text)
+        try:
+            await event.edit(previous_text)
+        except Exception as e:
+            logger.warn(str(e))
+            pass
         await asyncio.sleep(DELAY_BETWEEN_EDITS)
