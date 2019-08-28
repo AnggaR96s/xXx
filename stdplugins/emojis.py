prono69/UniMD

possibly Fix #23

stdplugins/emojis.py
@@ -9,7 +9,7 @@ import asyncio
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("emoji (.*)"))
+@borg.on(admin_cmd(pattern="emoji (.*)"))
 async def _(event):
     if event.fwd_from:
         return

stdplugins/get_bot.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantsBots
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("get_bot ?(.*)"))
+@borg.on(admin_cmd(pattern="get_bot ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return

stdplugins/translate.py
@@ -8,7 +8,7 @@ from googletrans import Translator
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("tr ?(.*)"))
+@borg.on(admin_cmd(pattern="tr ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return

AnggaR96s/xXx

stdplugins/emojis.py
@@ -9,7 +9,7 @@ import asyncio
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("e (.*)"))
+@borg.on(admin_cmd(pattern="e (.*)"))
 async def _(event):
     if event.fwd_from:
         return

stdplugins/get_bot.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantsBots
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("bot ?(.*)"))
+@borg.on(admin_cmd(pattern="bot ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return

stdplugins/translate.py
@@ -8,7 +8,7 @@ from googletrans import Translator
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("tl ?(.*)"))
+@borg.on(admin_cmd(pattern="tl ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return