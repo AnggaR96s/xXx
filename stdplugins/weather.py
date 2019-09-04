2512f47b4ded547cbb8eb55e2911cd57f71e2371
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-09-04 19:52:00 +0530

    rename to weather.py

--- stdplugins/weather.py
+++ stdplugins/weather.py
@@ -2,6 +2,7 @@
 Syntax: .weather <Location> """
 
 import aiohttp
+import io
 import time
 from datetime import tzinfo, datetime
 from uniborg.util import admin_cmd
@@ -47,3 +48,22 @@ async def _(event):
         )
     else:
         await event.edit(response_api["message"])
+
+
+@borg.on(admin_cmd(pattern="wttr (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sample_url = "https://wttr.in/{}.png"
+    # logger.info(sample_url)
+    input_str = event.pattern_match.group(1)
+    async with aiohttp.ClientSession() as session:
+        response_api_zero = await session.get(sample_url.format(input_str))
+        # logger.info(response_api_zero)
+        response_api = await response_api_zero.read()
+        with io.BytesIO(response_api) as out_file:
+            await event.reply(
+                file=out_file
+            )
+    await event.edit(input_str)
+
