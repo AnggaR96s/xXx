915c56c346fe9377ab56194a5383961eb86a07ad
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-09-19 21:35:33 +0530

    fix an undefined error

--- dbplugins/notification_mtab_manager.py
+++ dbplugins/notification_mtab_manager.py
@@ -217,7 +217,11 @@ async def on_new_chat_action_message(event):
 async def on_new_channel_message(event):
     if Config.PM_LOGGR_BOT_API_ID is None:
         return
-    if tgbot is None:
+    try:
+        if tgbot is None:
+            return
+    except Exception as e:
+        logger.info(str(e))
         return
     # logger.info(event.stringify())
     if isinstance(event, types.UpdateChannel):
