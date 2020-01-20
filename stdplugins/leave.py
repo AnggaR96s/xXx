1074829b2d210b7ac1339eadbd9f64faf79b1e12
Angga R. Saputra <kiritoclasher@gmail.com>
2020-01-20 15:27:56 +0700

    Update leave.py

--- stdplugins/leave.py
+++ stdplugins/leave.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 import time
 
 
-@borg.on(admin_cmd("leave", outgoing=True))
+@borg.on(admin_cmd(pattern="leave"))
 async def leave(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("`I am leaving this chat.....!`")
